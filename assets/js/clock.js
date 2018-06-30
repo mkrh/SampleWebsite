cbe87126472022359aee5500e7d4150256abbc1a
mkrh <mathiasklieber@gmail.com>
2018-06-30 19:22:46 +0200

    initial commit

---
+++ assets/js/clock.js
@@ -0,0 +1,13 @@
+const time = document.getElementById("time");
+startClock();
+
+function startClock() {
+  let today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+  time.innerHTML = h + ":" + m + ":" + s;
+  let le = setTimeout(startClock, 100);
+}
